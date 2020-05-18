0c22651ca1fd61256041b7f864c1a43bc9cc5ad6
shubhxcluzive <shubhxcluzive@gmail.com>
2020-05-18 17:54:03 +0530

    tasks

---
+++ Problems/Coordinates/task.py
@@ -0,0 +1,11 @@
+num1 = float(input())
+num2 = float(input())
+if num1 >= 0 and num2 > 0:
+    print("I")
+elif num1 <= 0 < num2:
+    print("II")
+elif num1 <= 0 and num2 < 0:
+    print("III")
+elif num1 >= 0 > num2:
+
+    print("IV")
